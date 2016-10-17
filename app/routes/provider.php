23e700772121d477939ffea4e05c345506fe8a59
d0ngix <jmbrothers@yahoo.com>
2016-10-17 13:41:18 +0800

    Signed-off-by: d0ngix <jmbrothers@yahoo.com>

--- app/routes/provider.php
+++ app/routes/provider.php
@@ -32,7 +32,7 @@ $app->get('/provider', function($request, $response, $args){
 			"( 3959 * acos( cos( radians($lat) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians($long) ) + sin( radians($lat) ) * sin( radians( latitude ) ) ) ) AS distance"
 		];
 		$selectStmt = $this->db->select($arrSelect)->from('vendor_addresses')
-													//->whereMany(['deleted' => '0', 'active' => '1'], '=')
+													->whereMany(['deleted' => '0', 'active' => '1'], '=')
 													->having('distance','<', $rad)
 													->orderBy('distance');
 		$selectStmt = $selectStmt->execute();
@@ -48,7 +48,7 @@ $app->get('/provider', function($request, $response, $args){
 		}
 
 		//Get the vendors list
-		$selectStmt = $this->db->select()->from('vendors')->whereIn('id',$intVendorId)->where('deleted','=','0');		
+		$selectStmt = $this->db->select()->from('vendors')->whereIn('id',$intVendorId);//->where('deleted','=','0');		
 		$selectStmt = $selectStmt->execute();
 		$arrResult = $selectStmt->fetchAll();		
 		
